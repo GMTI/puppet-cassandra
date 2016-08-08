01c6b07c987eb73edf221a080038e484f06627e7
Jim Hayner <jhayner@gannett.com>
2016-08-08 17:48:13 -0400

    Update install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -33,22 +33,22 @@ class cassandra::install {
 
   if $::osfamily == 'Debian' {
     package {'cassandra':
-      ensure  => '2.0.17',
+      ensure  => '2.2.7-1',
       require => [Class['java'], Exec['update-cassandra-repos']],
       before  => Package['dsc20']
     }
   }
 
-  package {'dsc20':
-    ensure  => '2.0.17-1',
+  package {'dsc22':
+    ensure  => '2.2.7-1',
     require => [Class['java'], Exec['update-cassandra-repos']]
   }
 
   file {'/usr/share/cassandra/apache-cassandra.jar':
     ensure  => link,
-    target  => '/usr/share/cassandra/apache-cassandra-2.0.17.jar',
+    target  => '/usr/share/cassandra/apache-cassandra-2.2.7.jar',
     owner   => 'cassandra',
     group   => 'cassandra',
-    require => Package['dsc20']
+    require => Package['dsc22']
   }
 }
